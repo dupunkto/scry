88da678d0ee41b4638fc4cbc5f3ce40f13a3d1b3
Robin <robindev2019@outlook.com>
2025-05-01 23:27:57 +0200

    Use new format + fix read bug for new shards

--- app.py
+++ app.py
@@ -9,31 +9,39 @@ app = Flask(__name__)
 def index():
     return text("words of prophets are\nwritten on the subway walls.")
 
-@app.route("/watch/<token>", methods=["POST"])
-def watch(token):
+@app.route("/track/<token>", methods=["POST"])
+def track(token):
     if token == os.getenv("TOKEN"):
         data = request.json
-        slug = data.get("slug")
-        source = data.get("source_code")
-        
-        if slug and source:
-            if ".." in slug or "'" in slug:
-                return text("Bad request.", 400)
-            else:
-                repo = os.getenv("ROOT")
-                path = os.path.join(repo, f"{slug}.ex")
+        change = data.get("change")
 
-                with open(path, 'r') as ref:
-                    old_source = ref.read()
-
-                with open(path, 'w+') as ref:
-                    ref.write(source)
-
-                if old_source.strip() == source.strip():
-                    return text("Not changed.", 200)
+        if change:
+            slug = change.get("slug")
+            source = change.get("source_code")
+            
+            if slug and source:
+                if ".." in slug or "'" in slug:
+                    return text("Bad request.", 400)
                 else:
-                    subprocess.check_output(f"git add . && git commit -m 'Edit {slug}'", cwd=repo, shell=True)
-                    return text("Committed.", 200)
+                    repo = os.getenv("ROOT")
+                    path = os.path.join(repo, f"{slug}.ex")
+
+                    try:
+                        with open(path, 'r') as ref:
+                            old_source = ref.read()
+                    except FileNotFoundError:
+                        old_source = ""
+
+                    with open(path, 'w+') as ref:
+                        ref.write(source)
+
+                    if old_source.strip() == source.strip():
+                        return text("Not changed.", 200)
+                    else:
+                        subprocess.check_output(f"git add . && git commit -m 'Edit {slug}'", cwd=repo, shell=True)
+                        return text("Committed.", 200)
+            else:
+                return text("Bad request.", 400)
         else:
             return text("Bad request.", 400)
     else:
