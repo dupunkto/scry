75b1a58af57c2ca08758e15b779fbb84073dd1a8
Robin <robindev2019@outlook.com>
2025-05-01 22:18:13 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,47 @@
+from flask import Flask, Response, abort, request, jsonify
+
+import subprocess
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return text("words of prophets are\nwritten on the subway walls.")
+
+@app.route("/watch/<token>", methods=["POST"])
+def watch(token):
+    if token == os.getenv("TOKEN"):
+        data = request.json
+        slug = data.get("slug")
+        source = data.get("source_code")
+        
+        if slug and source:
+            if ".." in slug or "'" in slug:
+                return text("Bad request.", 400)
+            else:
+                repo = os.getenv("ROOT")
+                path = os.path.join(repo, f"{slug}.ex")
+
+                with open(path, 'r') as ref:
+                    old_source = ref.read()
+
+                with open(path, 'w+') as ref:
+                    ref.write(source)
+
+                if old_source.strip() == source.strip():
+                    return text("Not changed.", 200)
+                else:
+                    subprocess.check_output(f"git add . && git commit -m 'Edit {slug}'", cwd=repo, shell=True)
+                    return text("Committed.", 200)
+        else:
+            return text("Bad request.", 400)
+    else:
+        return text("Forbidden.", 403)
+
+def text(message, status=200):
+    return Response(message, mimetype="text/plain", status=status)
+
+# Fucking Python. Doe ff normaal man.
+if __name__ == "__main__":
+    app.run(debug=True, port=4000)
