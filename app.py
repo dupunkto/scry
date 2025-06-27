f04db48217b197bf36a0c5f36087749bd45420a9
Gijs <gijstenberg@gmail.com>
2025-06-27 21:26:01 +0200

    Robin is a bit stupid <3

--- app.py
+++ app.py
@@ -9,44 +9,45 @@ app = Flask(__name__)
 def index():
     return text("words of prophets are\nwritten on the subway walls.")
 
-@app.route("/track/<token>", methods=["POST"])
-def track(token):
+@app.route("/track/<slug>", methods=["POST"])
+def track(slug):
+    data = request.json
+    token = data.get("token")
+    
     if token != os.getenv("TOKEN"):
         return text("Forbidden.", 403)
+    
+    change = data.get("change")
+
+    if not change: 
+        return text("Bad request.", 400)
+
+    source = change.get("source_code")
+    
+    if not slug or not source:
+        return text("Bad request.", 400)
+    
+    if ".." in slug or "'" in slug:
+        return text("Bad request.", 400)
+
+    repo = os.getenv("ROOT")
+    path = os.path.join(repo, f"{slug}.ex")
+
+    try:
+        with open(path, "r") as ref:
+            old_source = ref.read()
+    except FileNotFoundError:
+        old_source = None
+
+    with open(path, "w") as ref:
+        ref.write(source)
+
+    if old_source.strip() == source.strip():
+        return text("Not changed.", 200)
     else:
-        data = request.json
-        change = data.get("change")
-
-        if not change:
-            return text("Bad request.", 400)
-        else:
-            slug = change.get("slug")
-            source = change.get("source_code")
-            
-            if not slug or not source:
-                return text("Bad request.", 400)
-            else:
-                if ".." in slug or "'" in slug:
-                    return text("Bad request.", 400)
-                else:
-                    repo = os.getenv("ROOT")
-                    path = os.path.join(repo, f"{slug}.ex")
-
-                    try:
-                        with open(path, "r") as ref:
-                            old_source = ref.read()
-                    except FileNotFoundError:
-                        old_source = None
-
-                    with open(path, "w") as ref:
-                        ref.write(source)
-
-                    if old_source.strip() == source.strip():
-                        return text("Not changed.", 200)
-                    else:
-                        subprocess.check_output(f"git add . && git commit -m 'Edited {slug}'", cwd=repo, shell=True)
-                        return text("Committed.", 200)
-            
+        subprocess.check_output(f"git add . && git commit -m 'Edited {slug}'", cwd=repo, shell=True)
+        return text("Committed.", 200)
+        
         
 
 def text(message, status=200):
